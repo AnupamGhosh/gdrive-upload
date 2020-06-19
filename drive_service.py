4891d542bcc29ebea6cda1544060542d6327c0d0
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2020-06-19 08:30:01 +0530

    init

---
+++ drive_service.py
@@ -0,0 +1,33 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+class GoogleDriveService():
+  def __init__(self):
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    creds = None
+    secrets_path = os.path.join(os.path.dirname(__file__), 'secrets')
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time
+    token_path = os.path.join(secrets_path, 'token.pickle')
+    if os.path.exists(token_path):
+      with open(token_path, 'rb') as token:
+        creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+      else:
+        secret = os.path.join(secrets_path, 'client_secret.json')
+        flow = InstalledAppFlow.from_client_secrets_file(secret, SCOPES)
+        creds = flow.run_local_server(port=0)
+      # Save the credentials for the next run
+      with open(token_path, 'wb') as token:
+        pickle.dump(creds, token)
+
+    self.drive_service = build('drive', 'v3', credentials=creds)
+
+  def get(self):
+    return self.drive_service
---
+++ drive_upload.py
@@ -0,0 +1,23 @@
+import os
+import logging
+
+from googleapiclient.http import MediaFileUpload
+
+from drive_service import GoogleDriveService
+
+class GoogleDriveUpload():
+  def __init__(self, drive_service: GoogleDriveService):
+    self.drive = drive_service.get()
+
+  def upload(self, path: str, drive_dir_id: str): # '1YnTQoIuk8J6780G-kLKdNZkuVZAAin04'
+    filename = os.path.basename(path)
+    file_metadata = {'name': filename, 'parents': [drive_dir_id]}
+    media = MediaFileUpload(path, resumable=True)
+    res = self.drive.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    logging.info('%s uploaded. FIle ID: %s', path, res.get('id'))
+
+
+path = os.path.join(os.path.dirname(__file__), 'file.txt')
+drive_service = GoogleDriveService()
+uploader = GoogleDriveUpload(drive_service)
+uploader.upload(path, '1YnTQoIuk8J6780G-kLKdNZkuVZAAin04')
